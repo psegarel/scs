e8c17f4890a23e46ca8af020112b13e6ed240541
Patrick Segarel <contact@patrick-segarel.com>
2016-01-20 23:38:03 +0800

    Basic Facebook integration

--- sites/all/themes/seatheme/js/seatheme.js
+++ sites/all/themes/seatheme/js/seatheme.js
@@ -1,5 +1,25 @@
 (function($){
 
+	Drupal.behaviors.facebook = {
+		attach: function( context , settings) {
+			window.fbAsyncInit = function() {
+				FB.init({
+				  appId      : '168382040195552',
+				  xfbml      : true,
+				  version    : 'v2.5'
+				});
+			};
+
+			(function(d, s, id){
+			 var js, fjs = d.getElementsByTagName(s)[0];
+				 if (d.getElementById(id)) {return;}
+				 js = d.createElement(s); js.id = id;
+				 js.src = "//connect.facebook.net/en_US/sdk.js";
+				 fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+		}
+	}
+
 	Drupal.behaviors.externalLinks = {
 		attach: function( context , settings) {
 
--- sites/all/themes/seatheme/templates/node.tpl.php
+++ sites/all/themes/seatheme/templates/node.tpl.php
@@ -104,6 +104,12 @@
       print render($content);
     ?>
   </div>
+  <br>
+  <div class="fb-like"
+    data-share="true"
+    data-width="450"
+    data-show-faces="true">
+  </div>
 
   <?php print render($content['links']); ?>
 
